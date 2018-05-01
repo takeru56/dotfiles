ea882b40ce78b9780b868b02ca7addae38f4d396
田中健 <takeru.tt5672@gmail.com>
2018-05-01 09:44:14 +0900

    Add fish

---
+++ config.fish
@@ -0,0 +1,21 @@
+# alias 
+alias td='cd ~/Desktop'
+alias ls='gls --color=auto'
+alias be='bundle exec'
+alias tp='cd ~/projects'
+alias dt='cd ~/dotfiles'
+alias nv='nvim'
+alias gs='git status'
+alias gc='git checkout'
+alias gpo='git push origin master'
+
+# ファイル作って入る
+function mkcd
+  mkdir $argv;
+  cd $argv;
+end
+
+# docker-compose "処理"
+function dc
+  command docker-compose $argv;
+end
