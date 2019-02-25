2c816726540486d0780d647c7fc1a1a1dedc9a66
田中健 <takeru.tt5672@gmail.com>
2019-02-25 14:13:37 +0900

    update

--- config.fish
+++ config.fish
@@ -4,11 +4,13 @@ alias ls='gls --color=auto'
 alias be='bundle exec'
 alias tp='cd ~/projects'
 alias dt='cd ~/dotfiles'
+alias tg='cd ~/projects3/go-pro/src/github.com/takeru56'
 alias nv='nvim'
 alias gs='git status'
 alias gc='git checkout'
 alias gb='git branch'
 alias gpo='git push origin master'
+alias gph='git push heroku master'
 
 # ファイル作って入る
 function mkcd
